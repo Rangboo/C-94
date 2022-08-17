d1405f0f18668657b53af452a21deaf6509048fc
Rangboo <93071040+Rangboo@users.noreply.github.com>
2022-08-17 21:05:43 +0800

    Add files via upload

---
+++ chesee_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAcSwMqXHTR0m8KdyujPjPzRRaxGbRvO2Y",
+    authDomain: "kwitter-5dc54.firebaseapp.com",
+    databaseURL: "https://kwitter-5dc54-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kwitter-5dc54",
+    storageBucket: "kwitter-5dc54.appspot.com",
+    messagingSenderId: "35789957756",
+    appId: "1:35789957756:web:0c1f288b3b094a2d5a77f9"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
